d7a1f344bd9ba852ef3ca1d0be524fdda1bc5ccf
Udi Levy <webdevudi@gmail.com>
2018-10-23 21:42:38 -0500

    added a mapping for the cardlist

--- src/CardList.js
+++ src/CardList.js
@@ -7,10 +7,18 @@ const CardList = ({coinObj}) => {
     // send data to the card
     return (
         <div className="container-fluid row">
-            <Card name={coinObj[coinsIdArray[0]].name} rank={coinObj[coinsIdArray[0]].rank} price={coinObj[coinsIdArray[0]].quotes["USD"].price} percentChange24={coinObj[coinsIdArray[0]].quotes["USD"].percent_change_24h} />
-            <Card name={coinObj[coinsIdArray[1]].name} rank={coinObj[coinsIdArray[1]].rank} price={coinObj[coinsIdArray[1]].quotes["USD"].price} percentChange24={coinObj[coinsIdArray[1]].quotes["USD"].percent_change_24h} />
-            <Card name={coinObj[coinsIdArray[2]].name} rank={coinObj[coinsIdArray[2]].rank} price={coinObj[coinsIdArray[2]].quotes["USD"].price} percentChange24={coinObj[coinsIdArray[2]].quotes["USD"].percent_change_24h} />
-            <Card name={coinObj[coinsIdArray[3]].name} rank={coinObj[coinsIdArray[3]].rank} price={coinObj[coinsIdArray[3]].quotes["USD"].price} percentChange24={coinObj[coinsIdArray[3]].quotes["USD"].percent_change_24h} />
+            {
+                coinsIdArray.map((number) => {
+                    return (
+                        <Card 
+                            name={coinObj[number].name} 
+                            rank={coinObj[number].rank} 
+                            price={coinObj[number].quotes["USD"].price} 
+                            percentChange24={coinObj[number].quotes["USD"].percent_change_24h} 
+                        />
+                    )
+                })  
+            }
         </div>
     )
 }
