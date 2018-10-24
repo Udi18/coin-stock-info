cf2ba666baa1466d4a9703d7f85965da2a5c511c
Earle-Poole <eepoole4@gmail.com>
2018-10-24 17:32:19 -0500

    Checkmate

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, rank, price, percentChange24, logoImg, percentChange7D}) => {
     // destructering the data from api to be used.
     return (
-        <div className="card shadow" style={{height: '340px', width: '225px', margin: '.75rem', marginBottom: '1rem', paddingBottom: '6rem', paddingTop: '1rem'}}>
+        <div className="card shadow align-baseline" style={{height: '340px', width: '225px', margin: '.75rem', marginBottom: '1rem', paddingBottom: '6rem', paddingTop: '1rem'}}>
             <img alt='coin/stock logo' style={{ height: '90px', width: '70px'}} src={`${logoImg}`} className="mx-auto card-img-top" />
             <div className="card-body">
                 <h2 className="card-title text-center">{name}</h2>
