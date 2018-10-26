b81b5dfd94bf1e8233afe8e8993c183032da7d29
Udi Levy <webdevudi@gmail.com>
2018-10-26 14:40:56 -0500

    Made it work!!!!

--- src/TitleBar.js
+++ src/TitleBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TitleBar = () => {
     return (
-    <nav class="navbar navbar-expand-lg navbar-light text-light bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light text-light bg-dark">
         <h1>Coin and Stocks</h1>
     </nav>
 )
