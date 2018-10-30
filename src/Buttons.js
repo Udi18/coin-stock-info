bdcd6546072893c9c60247c2bc81b5effdbc05fc
Earle Poole <44408506+Earle-Poole@users.noreply.github.com>
2018-10-30 17:03:05 -0500

    Reinstalled tachyons. added pointer and grow to buttons (#11)
    * Reinstalled tachyons. added pointer and grow to buttons
    
    * added flex-wrap to button div

--- src/Buttons.js
+++ src/Buttons.js
@@ -5,14 +5,14 @@ const Buttons = ({click, buttonIdObjectArr, activeButtons}) => {
     const activeButtonArray = activeButtons.map(obj => obj.coinId)
     const determineClassName = (num) => {
         if(activeButtonArray.includes(num)) {
-            return `list-group-item shadow-sm m-1 p-2 active`
+            return `list-group-item pointer nowrap grow shadow-sm m-1 p-2 active`
         } else {
-            return `list-group-item shadow-sm m-1 p-2`
+            return `list-group-item pointer nowrap grow shadow-sm m-1 p-2`
         }
     }
    
     return (
-        <div className="d-flex flex-row p-5 justify-content-center">
+        <div className="d-flex flex-row flex-wrap p-5 justify-content-center">
             {
                 buttonIdObjectArr.map((object) => {
                     return <button key={object.coinId} id={object.coinId} onClick={click} className={determineClassName(object.coinId)} >{object.name}</button>
